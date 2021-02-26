8e42225f833217931a59276b1c4df57ae1bb34e3
Divin Irakiza <irakizadivin@gmail.com>
2021-02-26 06:36:02 +0200

    ProductSubCategory DB migrations

---
+++ database/migrations/2021_02_26_042642_create_product_sub_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductSubCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_sub_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('category', 50);
+            $table->string('description', 255);
+            $table->foreign('product_category_id')->references('id')->on('product_categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_sub_categories');
+        Schema::table('product_categories', function(Blueprint $table)
+        {
+            $table->dropForeign('product_category_id');
+        });
+    }
+}
