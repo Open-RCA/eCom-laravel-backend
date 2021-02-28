21efbf341b2e6272180c0ecade0ffff3e9deaddf
Divin Irakiza <irakizadivin@gmail.com>
2021-02-28 19:13:32 +0200

    Model & Migration: ProductTable

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
 
-class Product extends Model
+class Product extends Eloquent
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'product_sub_category_id',
+        'unit_price',
+        'quantity'
+    ];
+
+    public $timestamps = true;
+
+//    public function productImages() {
+//        return $this->hasMany();
+//    }
+
+
 }
