806a0a23f6c38ed028d7e5440645fdd75cd9245e
Divin Irakiza <irakizadivin@gmail.com>
2021-03-11 10:01:39 +0200

    ProductRating Model

---
+++ app/Models/ProductRating.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductRating extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'rate',
+        'user_id',
+        'comment'
+    ];
+
+    public $timestamps = true;
+
+
+
+}
